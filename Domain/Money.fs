2e9d0e3b7cf51140d2c0ed4570d96aeb9f22bbd9
LettoL <Letto-l@yandex.ru>
2020-03-02 23:43:30 +0300

    init & sale & money modules

---
+++ Domain/Money.fs
@@ -0,0 +1,19 @@
+﻿module Domain.Money
+
+type OperationType = Cash | Cashless
+
+type Income = {
+    Sum: decimal
+    Type: OperationType
+    CashboxId: int
+}
+
+type Expense = {
+    Sum: decimal
+    Type: OperationType
+    CashboxId: int
+}
+
+type MoneyOperation =
+    | Income of Income
+    | Expense of Expense
---
+++ Domain/Sale.fs
@@ -0,0 +1,25 @@
+﻿module Domain.Sale
+
+open Domain.Money
+open System
+
+type SaleProduct = {
+    ProductId: int
+    SaleId: int
+}
+
+type Buyer =
+    | Regular
+    | Partner of int
+
+type MoneyInformation = {
+    Sum: decimal
+    Discount: decimal
+    Operations: seq<MoneyOperation>
+}
+
+type SaleFromStock = {
+    DateTime: DateTime
+    Comment: string
+    Products: seq<SaleProduct>    
+}
